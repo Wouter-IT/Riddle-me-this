6fc1c3e4f0cfd4402675f16fa25710edadeb5c7f
Wouter van den Dries <woutervddries@live.nl>
2023-07-12 20:57:25 +0000

    Add JavaScript logic to register the click on the Start! button

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+//Waits for the DOM to finish loading before running the game
+// Get the button elements  and add event listeners to hem
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "username") { //Collects the data-type value of the button that is clicked and checks if it it the confirm button.
+                createPlayer();
+                // runGame();
+            } else if (this.getAttribute("data-type") === "forfeit") { // if it's not submit it's one of the game modes, "this" takes the value of the button clicked (and it's data-type) and notifies the user accordingly
+                // returnMenu();
+            }
+            else {
+                // nextGame();
+            }
+        })
+    }
+})
+
+ function createPlayer() {
+    let playerName = document.getElementById('uname-input').value;
+    alert('The username is ' + playerName);
+}
