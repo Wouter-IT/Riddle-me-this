0eb030b6136ba5db8154ffe5e3ae9269424b00c0
Wouter van den Dries <woutervddries@live.nl>
2023-07-15 13:59:03 +0000

    Add code to retreive riddle from riddles to display on screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "username") { //Collects the data-type value of the button that is clicked and checks if it it the confirm button.
                 createPlayer();
-                startGame();
+                playGame();
             } else if (this.getAttribute("data-type") === "forfeit") { // if it's not submit it's one of the game modes, "this" takes the value of the button clicked (and it's data-type) and notifies the user accordingly
                 // returnMenu();
             }
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('uname-input').addEventListener("keydown", function(event) {
         if (event.key === "Enter") {
             createPlayer();
-            startGame();
+            playGame();
         }
     })    
 })
@@ -43,7 +43,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // Clears greeting messages and instruction from screen and displays html elements needed to play the game. 
 // Then generates 5 random numbers which are used to slect the riddles and gets the 1st of the 5 riddles. 
-function startGame() {
+function playGame() {
     document.getElementById('greeting-area').style.display = 'none';
     document.getElementById('username-input-area').style.display = 'none';
     let revealGame = document.getElementsByClassName('game-area');
@@ -53,11 +53,7 @@ function startGame() {
     // Calls a function to "select" 5 random numbers
     let rdmRiddleArray = [];
     rdmRiddleArray = riddleSelection();
-    alert(rdmRiddleArray[0]);
-    alert(rdmRiddleArray[1]);
-    alert(rdmRiddleArray[2]);
-    alert(rdmRiddleArray[3]);
-    alert(rdmRiddleArray[4]);
+    getRiddle(rdmRiddleArray[0]);    
 }
 
 // reuses code from the Lovemath project to generate 5 random numbers between 0 and 49 without repeating any number
@@ -97,84 +93,77 @@ function checkDouble(rdmNum, numArray) {
 }
 
 // get riddle moet: new riddle pakken
+function getRiddle(riddleNr) {
+    let number = riddleNr;
+    alert(number);
+    let selected = riddle[number];
+    alert(selected);
+// document.getElementById('riddle-imgage').innerHTML = selector.image;
+    document.getElementById('riddle-text').innerHTML = selector.text;
+    document.getElementById('riddle-hint').innerHTML = selector.hint;
+    let answer = selected.answer;
+}
+
 
 let riddle = [{
-    id: 1,
+    id: 0,
     image: '',
     text: 'Mr. Taylor has four daughters and each has a brother. In total, how many children does Mr. Taylor have?',
     answer: '5',
     hint: 'a number'
 }, {
-    id: 2,
+    id: 1,
     image: '',
     text: 'I possess a halo of water, walls of stone, and a tongue of wood. Long I have stood; what am I?',
     answer: 'castle',
     hint: 'a word'
 }, {
-    id: 3,
+    id: 2,
     image: '',
     text: 'What can run but never walk, have a mouth but never talk, have a head that never weeps, and a bed that never sleeps?',
     answer: 'river',
     hint: 'a word'
 }, {
-    id: 4,
+    id: 3,
     image: '',
     text: 'What has only two words, but thousands of letters?',
     answer: 'post office',
     hint: 'two words'
 }, {
-    id: 5,
+    id: 4,
     image: '',
     text: 'What can fill an entire room without taking up any space?',
     answer: 'light',
     hint: 'a word'
 }, {
-    id: 6,
+    id: 5,
     image: '',
     text: 'The first two letters signify a male, the first three letters signify a female, the first four letters signify a great person, while the entire word signifies a great woman.',
     answer: 'heroine',
     hint: 'a word'
 }, {
-    id: 7,
+    id: 6,
     image: '',
     text: 'What has ten letters and starts with gas?',
     answer: 'Automobile',
     hint: 'a word'
 }, {
-    id: 8,
+    id: 7,
     image: '',
     text: 'People in poverty have this. If you eat this you will die. What is it?',
     answer: 'nothing',
     hint: 'a word'
 }, {
-    id: 9,
+    id: 8,
     image: '',
     text: 'What has roots that no one sees and looms much taller than trees? Up it goes but yet it never grows; what is it?',
     answer: 'mountain',
     hint: 'a word'
 }, {
-    id: 10,
+    id: 9,
     image: '',
     text: 'What gets wet while drying?',
     answer: 'towel',
     hint: 'a word'
 }];
 
-
-// riddles[] rdl;
-// rdl = new riddles[50];
-
-// obj[0] = new riddle(23907,"Dell Laptop");  
-// obj[1] = new Product(91240,"HP 630");  
-// obj[2] = new Product(29823,"LG OLED TV");  
-// obj[3] = new Product(11908,"MI Note Pro Max 9");  
-// obj[4] = new Product(43590,"Kingston USB"); 
-
-
-// const riddle = {
-//     image: '',
-//     text: '',
-//     answer: ''
-
-
-// }
