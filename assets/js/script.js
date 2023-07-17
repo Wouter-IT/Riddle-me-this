53909409d7efd18232b91504f452f8a8ae4eff06
Wouter van den Dries <woutervddries@live.nl>
2023-07-17 21:44:48 +0000

    Add html in javascript to display all other icons with their according riddle.

--- assets/js/script.js
+++ assets/js/script.js
@@ -361,301 +361,301 @@ var leaderboardArray = new Array(10);
 // Array of objects that stores all riddles
 let riddle = [{
     id: 0,
-    image: '<img class="riddle-image"  src="assets/images/riddle-icons/riddle0.png" alt="icon of hand with 4 fingers up">',
+    image: '<img class="riddle-icon"  src="assets/images/riddle-icons/riddle0.png" alt="icon of hand with 4 fingers up">',
     text: 'Mr. Taylor has four daughters and each has a brother. In total, how many children does Mr. Taylor have?',
     answer: '5',
     hint: 'a number'
 }, {
     id: 1,
-    image: '<img class="riddle-image"  src="assets/images/riddle-icons/riddle1.png" alt="angel halo icon">',
+    image: '<img class="riddle-icon"  src="assets/images/riddle-icons/riddle1.png" alt="angel halo icon">',
     text: 'I possess a halo of water, walls of stone, and a tongue of wood. Long I have stood; what am I?',
     answer: 'castle',
     hint: 'a word'
 }, {
     id: 2,
-    image: '<img class="riddle-image"  src="assets/images/riddle-icons/riddle2.png" alt="running man icon"',
+    image: '<img class="riddle-icon"  src="assets/images/riddle-icons/riddle2.png" alt="running man icon"',
     text: 'What can run but never walk, have a mouth but never talk, have a head that never weeps, and a bed that never sleeps?',
     answer: 'river',
     hint: 'a word'
 }, {
     id: 3,
-    image: '<img class="riddle-image"  src="assets/images/riddle-icons/riddle3.png" alt="number 2 icon">',
+    image: '<img class="riddle-icon"  src="assets/images/riddle-icons/riddle3.png" alt="number 2 icon">',
     text: 'What has only two words, but thousands of letters?',
     answer: 'post office',
     hint: 'two words'
 }, {
     id: 4,
-    image: '<img class="riddle-image"  src="assets/images/riddle-icons/riddle4.png" alt="cube with questionmark icon">',
+    image: '<img class="riddle-icon"  src="assets/images/riddle-icons/riddle4.png" alt="cube with questionmark icon">',
     text: 'What can fill an entire room without taking up any space?',
     answer: 'light',
     hint: 'a word'
 }, {
     id: 5,
-    image: '<img class="riddle-image"  src="assets/images/riddle-icons/riddle5.png" alt="icon of man and woman">',
+    image: '<img class="riddle-icon"  src="assets/images/riddle-icons/riddle5.png" alt="icon of man and woman">',
     text: 'The first two letters signify a male, the first three letters signify a female, the first four letters signify a great person, while the entire word signifies a great woman.',
     answer: 'heroine',
     hint: 'a word'
 }, {
     id: 6,
-    image: '<img class="riddle-image" src="assets/images/riddle-icons/riddle6.png" alt="colourful number 10 icon">',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle6.png" alt="colourful number 10 icon">',
     text: 'What has ten letters and starts with gas?',
     answer: 'automobile',
     hint: 'a word'
 }, {
     id: 7,
-    image: '<img class="riddle-image" src="assets/images/riddle-icons/riddle7.png" alt="coin and downwards arrow icon">',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle7.png" alt="coin and downwards arrow icon">',
     text: 'People in poverty have this. If you eat this you will die. What is it?',
     answer: 'nothing',
     hint: 'a word'
 }, {
     id: 8,
-    image: '<img src="assets/images/riddle-icons/riddle8.png" alt="plant with roots icon">',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle8.png" alt="plant with roots icon">',
     text: 'What has roots that no one sees and looms much taller than trees? Up it goes but yet it never grows; what is it?',
     answer: 'mountain',
     hint: 'a word'
 }, {
     id: 9,
-    image: '<img src="assets/images/riddle-icons/riddle9.png" alt="person slipping icon">',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="person slipping icon">',
     text: 'What gets wet while drying?',
     answer: 'towel',
     hint: 'a word'
 }, {
     id: 10,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="chimney blowing smoke icon">',
     text: 'What is able to go up a chimney when down but unable to go down a chimney when up?',
     answer: 'umbrella',
     hint: 'a word'
 }, {
     id: 11,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="eraser icon">',
     text: 'What English word retains the same pronunciation, even after you take away four of its five letters?',
     answer: 'queue',
     hint: 'a word'
 }, {
     id: 12,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="girl jumping icon">',
     text: 'What jumps when walking and sits when standing?',
     answer: 'kangaroo',
     hint: 'a word'
 }, {
     id: 13,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="plate and cutlery icon">',
     text: 'If you eat me, my sender will eat you. What am I?',
     answer: 'fishhook',
     hint: 'a word'
 }, {
     id: 14,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="man shaving beard icon">',
     text: 'I can shave every day but my beard never changes. What am I?',
     answer: 'barber',
     hint: 'a word'
 }, {
     id: 15,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="doctor icon">',
     text: 'Three different doctors said that Paul is their brother yet Paul claims he has no brothers. Who is lying?',
     answer: 'nobody',
     hint: 'one word'
 }, {
     id: 16,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="confused man icon">',
     text: 'I exemplify a rare case where today comes before yesterday. What am I?',
     answer: 'dictionary',
     hint: 'a word'
 }, {
     id: 17,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="english dictionary icon">',
     text: 'Which English word is the odd one out: Stun, Ton, Evil, Letter, Mood, Bad, Strap, Snap, and Straw?',
     answer: 'letter',
     hint: 'a word'
 }, {
     id: 18,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="tall tree icon">',
     text: 'I am born tall and grow short with age. What could I be?',
     answer: 'pencil',
     hint: 'a word'
 }, {
     id: 19,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="confused person icon">',
     text: 'The person who makes it and the person who buys it have no use for it and the person who uses it never knows they are using it. What is it?',
     answer: 'coffin',
     hint: 'a word'
 }, {
     id: 20,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="female teacher icon">',
     text: 'My buddies and I were inseparable mates until one by one we were split. My teacher then gave me a smack on the head so off in the corner I sit. What am I?',
     answer: 'staple',
     hint: 'a word'
 }, {
     id: 21,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="person leaving icon">',
     text: 'The more these are taken, the more they are left behind. What are they?',
     answer: 'footstep',
     hint: 'a word'
 }, {
     id: 22,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="dead bird icon">',
     text: 'I eat to live and drink to die. What could I be?',
     answer: 'fire',
     hint: 'a word'
 }, {
     id: 23,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="cursing emoji icon">',
     text: 'I promise, I offend, I direct, and I fight. What am I?',
     answer: 'hand',
     hint: 'a word'
 }, {
     id: 24,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="man shaving beard icon">',
     text: 'I am a five-letter word. I sound the same when you remove my first letter. I sound the same when you remove my third letter. I sound the same when my last letter is removed and I sound the same when all three are removed. What word am I?',
     answer: 'empty',
     hint: 'a word'
 }, {
     id: 25,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="number 5 icon">',
     text: 'What is caught but never thrown?',
     answer: 'cold',
     hint: 'a word'
 }, {
     id: 26,
-    image: '',
-    text: 'When I am needed by you, you throw me away, but when I’m of no use, you take me back. What am I?',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="hjand throwing ball icon">',
+    text: 'When I am needed by you, you throw me away, but when I am of no use, you take me back. What am I?',
     answer: 'anchor',
     hint: 'a word'
 }, {
     id: 27,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="fly insect icon">',
     text: 'I fly without wings and cry without eyes. Wherever I lead, darkness follows. What could I be?',
     answer: 'cloud',
     hint: 'a word'
 }, {
     id: 28,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="hand refusing gift icon">',
     text: 'What is it that no one wants but no one wants to lose?',
     answer: 'lawsuit',
     hint: 'a word'
 }, {
     id: 29,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="hand open with 5 fingers icon">',
     text: 'I am a five-letter word and people eat me. If you remove the first letter I become an energy form. If you remove the first two letters, I am needed to live. Scramble the last three letters and I am a drink. What word am I?',
     answer: 'wheat',
     hint: 'a word'
 }, {
     id: 30,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="heart with bandages icon">',
     text: 'We hurt without moving and poison without touching. We bear truth and lies but are no judged by size. What are we?',
     answer: 'word',
     hint: 'a word'
 }, {
     id: 31,
-    image: '',
-    text: 'WMy first is in chocolate but no in ham. My second is in cake and also in jam. My third at tea-time is easily found. Altogether, this is a friend who is often around. What is it?',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="chocolate bar icon">',
+    text: 'My first is in chocolate but no in ham. My second is in cake and also in jam. My third at tea-time is easily found. Altogether, this is a friend who is often around. What is it?',
     answer: 'cat',
     hint: 'a word'
 }, {
     id: 32,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="timer icon">',
     text: 'What word becomes shorter when you add two letters to it?',
     answer: 'short',
     hint: 'a word'
 }, {
     id: 33,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="two persons together icon">',
     text: 'What word in the English language has three consecutive double letters?',
     answer: 'Bookkeeper',
     hint: 'a word'
 }, {
     id: 34,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="young girl icon">',
     text: 'A man is asked what his daughters look like. He answers, “they are all blondes, but two, all brunettes, but two, and all redheads, but two.” How many daughters does he have?',
     answer: 3,
     hint: 'a number'
 }, {
     id: 35,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="loop with questionmark icon">',
     text: 'You always find me in the past, I can be created in the present, but the future can never taint me. What am I?',
     answer: 'history',
     hint: 'a word'
 }, {
     id: 36,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="multiply X icon">',
     text: 'If you multiply me by any other number, the answer will always remain the same. What number am I?',
     answer: 0,
     hint: 'a number'
 }, {
     id: 37,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="three sugarcubes icon">',
     text: 'If there are three cups of sugar and you take one away, how many do you have?',
     answer: 1,
     hint: 'a number'
 }, {
     id: 38,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="hen on nest icon">',
     text: 'Two hens can lay two eggs in two minutes. If this is the maximum speed possible, what is the total number of hens needed to get 500 eggs in 500 minutes?',
     answer: 2,
     hint: 'a number'
 }, {
     id: 39,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="polarbear wearing scarf icon">',
     text: 'Paul the Polar Bear had a birthday party and had each guest bringing five fish. A penguin stole two fish but there were still 198 left. How many people came to the party?',
     answer: 40,
     hint: 'a number'
 }, {
     id: 40,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="copy icon">',
     text: 'Ten copycats were sitting on a boat when one jumped out. How many were left?',
     answer: 0,
     hint: 'a number'
 }, {
     id: 41,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="father and son icon">',
     text: 'If Tom and his father are put together, they weigh 280 pounds. Tom his dad weighs three times as much as he does. How much pounds does Tom weigh?',
     answer: 70,
     hint: 'a number'
 }, {
     id: 42,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="three questionmakrs icon">',
     text: 'I am a three-digit number. My tens digit is six more than my ones digit. My hundreds digit is eight less than my tens digit. What number am I?',
     answer: 193,
     hint: 'a number'
 }, {
     id: 43,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="cartboard box icon">',
     text: 'When shipping something, Tom can place ten small boxes or eight large boxes into a carton. A total of 96 boxes were sent in one shipment and the number of small boxes was less than large boxes. How many cartons did he ship?',
     answer: 11,
     hint: 'a number'
 }, {
     id: 44,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="two brothers icon">',
     text: 'Seven brothers were born two years apart. The youngest brother is seven. How old is the oldest brother?',
     answer: 19,
     hint: 'a number'
 }, {
     id: 45,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="3 blocks reading A B C icon">',
     text: 'Which letter of the alphabet has the most water?',
     answer: 'c',
     hint: 'a word'
 }, {
     id: 46,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="fragile broken glass icon">',
     text: 'What is so fragile that saying its name breaks it?',
     answer: 'silence',
     hint: 'a word'
 }, {
     id: 47,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="cheese icon">',
     text: 'What type of cheese is made backward?',
     answer: 'edam',
     hint: 'a word'
 }, {
     id: 48,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="three people growing up icon">',
     text: 'When asked how old she was, Suzie replied, “In two years I will be twice as old as I was five years ago.” How old is she?',
     answer: 12,
     hint: 'a number'
 }, {
     id: 49,
-    image: '',
+    image: '<img class="riddle-icon" src="assets/images/riddle-icons/riddle9.png" alt="4 cubes connected to each other icon">',
     text: 'What is it that when you take away the whole, you still have some leftover?',
     answer: 'wholesome',
     hint: 'a word'
